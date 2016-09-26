bdd1542d5e92fe820ad7d9fbfeee496cb8500827
Dave Gamberg <dgamberg@gmail.com>
2016-09-26 15:12:29 -0500

    object conversion

--- public/js/script.js
+++ public/js/script.js
@@ -1,23 +1,20 @@
 var globalVars = {
-    "characters": [
-        {
-            "name": "Warrior",
-            "health": 100,
-            "damage": 20,
-            "skillLevel": 5,
-            "imageUrl": "hero2.jpg"
-        }
-    ],
-    "monsters": [
-        { "id": 0, "name": "Big Spider", "health": 75, "damage": 5, "imageUrl": "spider.jpg", "skillLevel": 1 },
-        { "id": 1, "name": "Snake", "health": 58, "damage": 7, "imageUrl": "snake.jpg", "skillLevel": 2 },
-        { "id": 2, "name": "Orc", "health": 84, "damage": 10,"imageUrl": "orc.png", "skillLevel": 3},
-        { "id": 3, "name": "Goblin", "health": 95, "damage": 12, "imageUrl": "goblin.jpg", "skillLevel": 4 },
-        { "id": 4, "name": "Troll", "health": 120, "damage": 16, "imageUrl": "troll.jpg", "skillLevel": 5 }
+    "character": {
+        "name": "Warrior",
+        "health": 100,
+        "damage": 20,
+        "skillLevel": 5,
+        "imageUrl": "hero2.jpg"
+    },
+    "monsterList": [
+        { "name": "Big Spider", "damage": 5, "imageUrl": "spider.jpg", "skillLevel": 1},
+        { "name": "Snake",  "damage": 7, "imageUrl": "snake.jpg", "skillLevel": 2 },
+        { "name": "Orc",  "damage": 10,"imageUrl": "orc.png", "skillLevel": 3},
+        { "name": "Goblin", "damage": 12, "imageUrl": "goblin.jpg", "skillLevel": 4 },
+        { "name": "Troll",  "damage": 16, "imageUrl": "troll.jpg", "skillLevel": 5 }
     ],
     "currentMonster": {},
-    "monstersFought": [],
-    "lastNum":  null
+    "monstersKilled": []
 };
 
 var dungeonGame = {
@@ -25,16 +22,15 @@ var dungeonGame = {
         console.log("Game Initialized");
 
         //Load Hero
-        var hero = dungeonGame.loadHero(0);
-        dungeonGame.renderHeroCard(hero);
+        dungeonGame.renderCharacterCard(globalVars.character);
 
         // Get First Monster
         dungeonGame.getRandomMonster();
-        globalVars.currentMonster = globalVars.monstersFought[0];
-        dungeonGame.renderMonsterCard(globalVars.currentMonster);
+        dungeonGame.renderMonsterCard();
 
         $('.attack-button').on('click', function(){
-            dungeonGame.characterAttack(hero, globalVars.currentMonster );
+            var monster = this.getCurre
+            dungeonGame.characterAttack();
         });
 
         $('.continue-button').on('click', function(){
@@ -42,22 +38,11 @@ var dungeonGame = {
             $('.messages-container').html("");
             $('.attack-button-container').css('display', 'block');
             $('.continue-button-container').css('display', 'none');
-            var anotherMonster = dungeonGame.getRandomMonster();
-            // if last num is not null, this is not first run
-            if (globalVars.lastNum !== null){
-                //if same as last - re-do it
-                if(anotherMonster.id == globalVars.lastNum){
-                    // render monster
-                    anotherMonster = dungeonGame.getRandomMonster();
-                    dungeonGame.renderMonsterCard(anotherMonster);
-                }
-                else {
-                    dungeonGame.renderMonsterCard(anotherMonster);
-                }
 
-            }
+            dungeonGame.getRandomMonster();
             // first monster
-            dungeonGame.renderMonsterCard(anotherMonster);
+            dungeonGame.renderMonsterCard();
+
         });
     },
 
@@ -66,20 +51,48 @@ var dungeonGame = {
     // ------------------------------------
 
     getRandomMonster: function(){
-        globalVars.currentMonster = {};
-        var range = globalVars.monsters.length;
-        globalVars.lastNum = null;
-        var randNum = Math.floor((Math.random() * range));
+        this.setCurrentMonster({});
+        var range = globalVars.monsterList.length;
+        var randMonster = Math.floor((Math.random() * range));
 
-        var monster = globalVars.monsters[randNum];
-        monster.id = dungeonGame.generateMonsterId();
+        var monster = this.loadMonsterById(randMonster);
+        monster.id = this.generateMonsterId();
+
+        var monsterHealth = this.generateHealth(monster.skillLevel);
+        monster.health = monsterHealth;
+        this.setCurrentMonster(monster);
+    },
+    generateHealth: function (level) {
+        var min = 20;
+        var max = level * 20;
+        var healthToReturn =  Math.random() * (max - min) + min;
+        return healthToReturn.toFixed(0);
+    },
+    generateMonsterId: function(){
+        return globalVars.monstersKilled.length + 1;
+    },
+    setMonsterHealth: function(health){
+        console.log('Health set to ', health)
+        globalVars.currentMonster.health = health;
+    },
+    getMonsterHealth: function(){
+        console.log("Monsters", globalVars.monsters);
+        console.log("Current Monster", this.getCurrentMonster());
+        return globalVars.currentMonster.health;
+    },
+    getCurrentMonster: function(){
+        return globalVars.currentMonster;
+    },
+    setCurrentMonster: function(monster){
         globalVars.currentMonster = monster;
-        globalVars.monstersFought.unshift(monster);
-        console.log("Current Monster", globalVars.currentMonster);
-        console.log("Monsters Killed",globalVars.monstersFought);
+
+    },
+    loadMonsterById: function(position){
+        var monster = globalVars.monsterList[position];
+        return monster;
     },
     renderMonsterCard: function(){
-        var card = globalVars.currentMonster;
+        var card = this.getCurrentMonster();
         var output = "";
         output += "<div class='monster-card-container'><div class='monster-card-name-container'><div class='monster-card-name-value'>" + card.name + "</div></div>";
         output += "<div class='monster-card-stat-container'><div class='monster-card-stat-label'>Health:</div><div class='monster-card-health-value'>" + card.health + "</div></div>";
@@ -89,14 +102,10 @@ var dungeonGame = {
         var $el = $('.monster-card-output').children().last();
         $el.append(output);
     },
-    generateMonsterId: function(){
-        return globalVars.monstersFought.length + 1;
-    },
 
-    loadHero: function(number){
-        return globalVars.characters[number];
-    },
-    renderHeroCard: function(cardObject){
+
+
+    renderCharacterCard: function(cardObject){
         var output = "";
         output += "<div class='card-container'><div class='card-name-container'><div class='card-name-value'>" + cardObject.name + "</div></div>";
         output += "<div class='card-stat-container'><div class='card-stat-label'>Health:</div><div class='card-health-value'>" + cardObject.health + "</div></div>";
@@ -111,8 +120,9 @@ var dungeonGame = {
         var $el = $('.messages-container').children().last();
         $el.append(message);
     },
-    characterAttack: function(character, monster){
-        var heroSkill = character.skillLevel;
+    characterAttack: function(){
+        var heroSkill = globalVars.character.skillLevel;
+        var monster = this.getCurrentMonster();
         var heroMultiplier = 10 * heroSkill;
         var range = 100;
         var randNum = Math.floor((Math.random() * range) +1);
@@ -120,40 +130,45 @@ var dungeonGame = {
         // console.log("hero Skill" , heroMultiplier);
         if (randNum <= heroMultiplier){
             dungeonGame.addMessage("<div class='damaged'>You Hit The "+ monster.name +" </div>");
-            dungeonGame.damageMonster(monster, character.damage);
+            dungeonGame.damageMonster();
         }
         else {
             dungeonGame.addMessage("<div class='missed'>You Missed</div>");
         }
     },
     updateCurrentMonsterHealth: function(health){
-        globalVars.currentMonster.health = health;
+        this.setMonsterHealth(health);
         $('.monster-card-health-value').text(health);
     },
-    damageMonster: function(monster, damage){
-        var newHealth = monster.health - damage;
+    damageMonster: function(){
+        var damage = globalVars.character.damage;
+        var health = this.getMonsterHealth();
+        var newHealth = health - damage;
+        this.setMonsterHealth(newHealth);
+
         dungeonGame.addMessage("<div class='damaged'>You Did " + damage + " damage! </div>");
-        dungeonGame.addMessage("<div class='health'>" + globalVars.currentMonster.health + " health remaining</div>");
+        dungeonGame.addMessage("<div class='health'>" + newHealth + " health remaining</div>");
         dungeonGame.updateCurrentMonsterHealth(newHealth);
         if (newHealth <= 0){
             if(newHealth < 0){
-                globalVars.currentMonster.health = 0;
-
+                this.setMonsterHealth(0);
             }
-            dungeonGame.monsterIsDead(globalVars.currentMonster);
+            dungeonGame.monsterIsDead();
         }
         else{
-            $('.monster-card-health-value').text(globalVars.currentMonster.health);
+            $('.monster-card-health-value').text(newHealth);
         }
     },
 
-    monsterIsDead: function(monster){
+    monsterIsDead: function(){
+        var monster = this.getCurrentMonster();
         $('.monster-card-health-value').html("<span class='dead'>DEAD</span>");
         var message = "";
         message += "<div class='dead'>The " + monster.name + " is dead</div>";
         dungeonGame.addMessage(message);
         $('.attack-button-container').css('display', 'none');
         $('.continue-button-container').css('display', 'block');
+        globalVars.monstersKilled.unshift(monster);
     }
 };
 $(document).ready(function(){
